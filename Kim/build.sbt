5f725db1c27ac720c134559e726e142fcc548001
donghwankim <benetori@gmail.com>
2017-05-21 16:54:28 +0900

    개발 환경 셋팅

---
+++ Kim/build.sbt
@@ -0,0 +1,6 @@
+name := "Kim"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+        
--- Lee/src/test/scala/test.scala
+++
@@ -1,8 +0,0 @@
-/**
-  * Created by donghwankim on 2017. 5. 21..
-  */
-object test {
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-  }
-}
