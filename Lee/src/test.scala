cf503cd17231f592e9ec8dc25e62e69b341d98f2
Subin Lee <sblee122s85@gmail.com>
2017-05-28 15:46:13 +0900

    foreach 테스트
    2장의 for 구문을 테스트

--- Lee/src/test.scala
+++ Lee/src/test.scala
@@ -1,5 +1,13 @@
 object test {
   def main(args: Array[String]): Unit = {
-    println("Hello, World2")
+    var test = Map(
+        "testkey1" -> "value", "testkey2" -> "value2",
+        "testkey3" -> "value3", "testkey4" -> "value4",
+        "testkey5" -> "value5"
+        )
+        
+    for (i <- test) {
+      println(i)
+    }
   }
 }
